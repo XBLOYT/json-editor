3643632323340b62c91946574c953413c6661049
Xavi <xavierbern2@gmail.com>
2025-08-26 22:17:12 +0200

    chore: initial Maven project (Java 17) + gitignore + readme

---
+++ src/test/java/com/xbl/jsoneditor/AppTest.java
@@ -0,0 +1,20 @@
+package com.xbl.jsoneditor;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
